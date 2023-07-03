aae49e6c2d6080c978eda84eb4572ef534af19a2
wonjangcloud9 <venuscwj@naver.com>
2023-07-03 20:45:58 +0900

    btn

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   let post = "오사카 소바 맛집";
   let [글제목, b] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬독학"]);
   let [logo, setLogo] = useState("ReactBlog");
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
@@ -14,7 +15,17 @@ function App() {
       {글제목.map(function (글) {
         return (
           <div className="list">
-            <h4>{글}</h4>
+            <h4>
+              {글}
+              <span
+                onClick={() => {
+                  따봉변경(따봉 + 1);
+                }}
+              >
+                👍
+              </span>
+              {따봉}
+            </h4>
             <p>7월 3일 발행</p>
           </div>
         );
