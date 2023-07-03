15189e0d49d473ce31c25a7378f72b3416ae1226
wonjangcloud9 <venuscwj@naver.com>
2023-07-03 20:38:20 +0900

    모든 상태를 state로 관리하지는 않아도 되는구나...

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
   let post = "오사카 소바 맛집";
+  let [글제목, b] = useState("여자 코트 추천");
+  let [logo, setLogo] = useState("ReactBlog");
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>{logo}</h4>
       </div>
-      <h4
-        style={{
-          fontSize: "30px",
-        }}
-      >
-        {post}
-      </h4>
+      <div className="list">
+        <h4>{글제목}</h4>
+        <p>7월 3일 발행</p>
+      </div>
     </div>
   );
 }
