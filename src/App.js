b68ecf79a42846faf1f29ac1d32de3f697e25c24
wonjangcloud9 <venuscwj@naver.com>
2023-07-03 20:40:07 +0900

    stateList

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 
 function App() {
   let post = "오사카 소바 맛집";
-  let [글제목, b] = useState("여자 코트 추천");
+  let [글제목, b] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬독학"]);
   let [logo, setLogo] = useState("ReactBlog");
 
   return (
@@ -11,10 +11,14 @@ function App() {
       <div className="black-nav">
         <h4>{logo}</h4>
       </div>
-      <div className="list">
-        <h4>{글제목}</h4>
-        <p>7월 3일 발행</p>
-      </div>
+      {글제목.map(function (글) {
+        return (
+          <div className="list">
+            <h4>{글}</h4>
+            <p>7월 3일 발행</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
