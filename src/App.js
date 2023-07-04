949605626262e753cf49a55594612ee3dc0e7ac4
wonjangcloud9 <venuscwj@naver.com>
2023-07-04 23:33:27 +0900

    component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React, { useState } from "react";
 
 function App() {
   let post = "오사카 소바 맛집";
-  let [글제목, b] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬독학"]);
+  let [글제목, 글제목변경] = useState([
+    "남자 코트 추천",
+    "강남 우동맛집",
+    "파이썬독학",
+  ]);
   let [logo, setLogo] = useState("ReactBlog");
   let [따봉, 따봉변경] = useState(0);
 
@@ -25,13 +29,27 @@ function App() {
                 👍
               </span>
               {따봉}
+              <button
+                onClick={() => {
+                  let newArray = [...글제목];
+                  newArray.sort();
+                  글제목변경(newArray);
+                }}
+              >
+                List 변경
+              </button>
             </h4>
             <p>7월 3일 발행</p>
+            <Footer />
           </div>
         );
       })}
     </div>
   );
+}
+
+function Footer() {
+  return <div>작성자 - 원장</div>;
 }
 
 export default App;
